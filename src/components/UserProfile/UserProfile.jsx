aa5c377442c6a4c922f52b9d63de839e979edcfe
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-12-31 18:00:06 +0530

    feat: make the user profile dynamic

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -1,9 +1,24 @@
+import dropdownArrow from "../../../public/assets/dropdown-arrow.svg";
 import styles from "./UserProfile.module.css";
 
-function UserProfile() {
-    <span className={styles.userProfile}>
+function UserProfile({ userAvatar, userName }) {
+    return (
+        <div className={styles.userProfile}>
+            <img 
+                className={styles.userAvatar} 
+                src={userAvatar} 
+                alt={`${userName}'s profile picture`} 
+            />
+            
+            <span className={styles.userName}>
+                {userName}
+            </span>
 
-    </span>
+            <button className={styles.dropdownArrow}>
+                <img src={dropdownArrow} alt="Dropdown arrow" />
+            </button>
+        </div>
+    );
 }
 
 export default UserProfile;
