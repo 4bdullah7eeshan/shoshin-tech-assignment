928ec452c6fdb01cfb917addba9ca5a6c135ff45
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-12-31 13:46:19 +0530

    style: setup alignment of combination mark items

--- src/components/CombinationMark/CombinationMark.jsx
+++ src/components/CombinationMark/CombinationMark.jsx
@@ -1,9 +1,10 @@
 import Logo from "../Logo/Logo";
 import Wordmark from "../Wordmark/Wordmark";
+import { styles } from "./CombinationMark.module.css";
 
 function CombinationMark() {
     return (
-        <div>
+        <div className={styles.combinationMark}>
             <Logo />
             <Wordmark />
         </div>
