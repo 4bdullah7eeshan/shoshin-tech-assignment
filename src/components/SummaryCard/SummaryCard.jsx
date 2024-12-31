03be9e3a5e432a686361b79c07b2bd5abca032b3
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-12-31 23:20:51 +0530

    feat: setup a dynamic summary card

--- src/components/SummaryCard/SummaryCard.jsx
+++ src/components/SummaryCard/SummaryCard.jsx
@@ -1,13 +1,36 @@
+import upArrow from "../../../public/assets/up-arrow.svg";
+import graph from "../../../public/assets/graph.svg";
+import graphGradient from "../../../public/assets/graph-gradient.svg";
+
 import styles from "./SummaryCard.module.css";
 
-function SummaryCard() {
+function SummaryCard({ title, info, menCount, womenCount, isIncrease, increasePercentage }) {
     return(
         <article>
             <section>
-                {/* Textual content */}
+                <header>
+                    <h4>{title}</h4>
+                    <p>{info}</p>
+                </header>
+
+                <footer>
+                    <p><small>{`${menCount} Men`}</small></p>
+                    <p><small>{`${womenCount} Women`}</small></p>
+                </footer>
             </section>
             <section>
-                {/* Graph */}
+                <div>
+                    <small>
+                        {isIncrease ? '+' : '-'}{increasePercentage}%
+                    </small>
+                    <img src={upArrow} alt="" />
+                    <img src={graph} alt="" />
+                    <img src={graphGradient} alt="" />
+                </div>
+
+                <p>
+                    <small>{isIncrease ? '+' : '-'}{increasePercentage}% Past month</small>
+                </p>
             </section>
         </article>
     )
