0e271b6245d2b96cdd7ffccae25535eff75994a5
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-12-31 14:37:07 +0530

    chore: gather menu items

---
+++ src/utils/data/mainMenuItems.js
@@ -0,0 +1,16 @@
+import dashboardIcon from "../../../public/assets/dashboard-icon.svg";
+import recruitmentIcon from "../../../public/assets/recruitment-icon.svg";
+import scheduleIcon from "../../../public/assets/schedule-icon.svg";
+import employeeIcon from "../../../public/assets/employee-icon.svg";
+import departmentIcon from "../../../public/assets/department-icon.svg";
+
+
+const mainMenuItems = [
+    { icon: {dashboardIcon}, text: "Dashboard" },
+    { icon: {recruitmentIcon}, text: "Recruitment" },
+    { icon: {scheduleIcon}, text: "Schedule" },
+    { icon: {employeeIcon}, text: "Employee" },
+    { icon: {departmentIcon}, text: "Department" },
+];
+
+export default mainMenuItems;
---
+++ src/utils/data/otherMenuItems.js
@@ -0,0 +1,9 @@
+import supportIcon from "../../../public/assets/support-icon.svg";
+import settingsIcon from "../../../public/assets/settings-icon.svg";
+
+const otherMenuItems = [
+    { icon: {supportIcon}, text: "Support" },
+    { icon: {settingsIcon}, text: "Settings" },
+];
+
+export default otherMenuItems;
