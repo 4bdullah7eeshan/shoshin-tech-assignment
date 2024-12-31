935bd5ce8e17d4526344b209585cd93883b37a9e
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-12-31 14:51:37 +0530

    refactor: improve semantics

--- src/components/MenuSection/MenuSection.jsx
+++ src/components/MenuSection/MenuSection.jsx
@@ -2,14 +2,14 @@ import MenuItem from "../MenuItem/MenuItem";
 
 const MenuSection = ({ title, items }) => {
     return (
-        <li>
+        <section>
             <h3>{title}</h3>
             <ul>
                 {items.map((item, index) => (
                     <MenuItem key={index} icon={item.icon} text={item.text} />
                 ))}
             </ul>
-        </li>
+        </section>
     );
 };
 
