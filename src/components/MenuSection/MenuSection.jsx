46b2ce52da7bc926ca539592304e6442e8897991
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-12-31 16:33:20 +0530

    style: improve overall sidebar based on specs
    - Applied styles to the menu titles.
    - Gathered Roboto font to self host it.
    - Improved layout and alignment of Menus and Sidebar

--- src/components/MenuSection/MenuSection.jsx
+++ src/components/MenuSection/MenuSection.jsx
@@ -1,9 +1,10 @@
 import MenuItem from "../MenuItem/MenuItem";
+import styles from "./MenuSection.module.css";
 
 const MenuSection = ({ title, items }) => {
     return (
-        <section>
-            <h3>{title}</h3>
+        <section className={styles.menuSection}>
+            <p className={styles.menuTitle}>{title}</p>
             <ul>
                 {items.map((item, index) => (
                     <MenuItem key={index} icon={item.icon} text={item.text} />
--- src/components/Menus/Menus.jsx
+++ src/components/Menus/Menus.jsx
@@ -1,11 +1,12 @@
 import MenuSection from "../MenuSection/MenuSection";
 import mainMenuItems from "../../utils/data/mainMenuItems";
 import otherMenuItems from "../../utils/data/otherMenuItems";
+import styles from "./Menus.module.css";
 
 const Menus = () => {
     return (
       <nav className="menu">
-        <ul>
+        <ul className={styles.menusContainer}>
           <MenuSection title="Main Menu" items={mainMenuItems} />
           <MenuSection title="Other" items={otherMenuItems} />
         </ul>
