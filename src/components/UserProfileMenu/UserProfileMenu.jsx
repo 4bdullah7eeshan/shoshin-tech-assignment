b8df0450eae2a5e662f5edeeb25c68a3d4619783
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-12-31 17:25:07 +0530

    feat: create and setup layout of user profile menu

---
+++ src/components/UserProfileMenu/UserProfileMenu.jsx
@@ -0,0 +1,11 @@
+import styles from "./UserProfileMenu.module.css";
+
+function UserProfileMenu() {
+    return (
+        <header className={styles.userProfileMenu}>
+
+        </header>
+    )
+}
+
+export default UserProfileMenu;
