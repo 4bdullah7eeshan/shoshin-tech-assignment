1073f65fb379b39348669138bff41511a4876c85
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-12-31 22:16:12 +0530

    feat: create a function to handle dynamic styling
    Based on the card's title, this function will setup the bg color and desc text color.

--- src/components/InfoCard/InfoCard.jsx
+++ src/components/InfoCard/InfoCard.jsx
@@ -1,11 +1,25 @@
 import styles from "./InfoCard.module.css";
 
-function InfoCard() {
-    <article className={styles.infoCard}>
-        <p></p>
-        <p></p>
-        <p></p>
-    </article>
+function InfoCard({ title, info, description }) {
+    
+    const getCardStyles = () => {
+        switch (title) {
+          case "Available Position":
+            return { bgColor: "#FFEFE7", descColor: "#FF5151" };
+          case "Job Open":
+            return { bgColor: "#E8F0FB", descColor: "#3786F1" };
+          case "New Employees":
+            return { bgColor: "#FDEBF9", descColor: "#EE61CF" };
+        }
+    };
+
+    return (
+        <article className={styles.infoCard}>
+            <p></p>
+            <p></p>
+            <p></p>
+        </article>
+    );
 }
 
 export default InfoCard;
