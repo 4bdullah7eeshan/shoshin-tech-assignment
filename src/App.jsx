7533b61e576ecfe36f4b7175375ad5d60be5c8fb
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-12-31 16:58:14 +0530

    wip: setup main app structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import Sidebar from "./components/Sidebar/Sidebar"
+import Sidebar from "./components/Sidebar/Sidebar";
+import Main from "./components/Main/Main";
+import "./App.css";
+
 function App() {
 
   return (
-    <>
+    <div id="home">
       <Sidebar />
-      
-    </>
+      <Main />      
+    </div>
   )
 }
 
