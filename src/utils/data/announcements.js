c0540f9c9e62d72820796191a3b844c351c305c3
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2025-01-01 01:10:50 +0530

    chore: setup announcements data

---
+++ src/utils/data/announcements.js
@@ -0,0 +1,16 @@
+const announcements = [
+  {
+    title: "Outing schedule for every departement",
+    time: "5 Minutes ago",
+  },
+  {
+    title: "Meeting HR Department",
+    time: "Yesterday, 12:30 PM",
+  },
+  {
+    title: "IT Department need two more talents for UX/UI Designer position",
+    time: "Yesterday, 09:15 AM",
+  },
+];
+
+export default announcements;
