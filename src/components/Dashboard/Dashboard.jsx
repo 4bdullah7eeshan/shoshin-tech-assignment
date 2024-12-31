339c6cc0372aa7658bd9547604f81cd28f53ff39
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-12-31 21:00:23 +0530

    feat: setup dashboard structure

---
+++ src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,52 @@
+import styles from "./Dashboard.module.css";
+
+function Dashboard() {
+    return(
+        <main className={styles.dashboard}>
+            <header>
+                <h1>Dashboard</h1>
+            </header>
+            <div>
+                <div>
+                    <div>
+                        <div>
+
+                        </div>
+                        <div>
+
+                        </div>
+                        <div>
+                            
+                        </div>
+                    </div>
+
+                    <div>
+                        <div>
+
+                        </div>
+                        <div>
+
+                        </div>
+                    </div>
+
+                    <div>
+
+                    </div>
+
+                </div>
+                <div>
+                    <div>
+
+                    </div>
+                    <div>
+                        
+                    </div>
+
+                </div>
+            </div>
+
+        </main>
+    );
+}
+
+export default Dashboard;
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,13 @@
 import Navbar from "../Navbar/Navbar";
+import Dashboard from "../Dashboard/Dashboard";
+
 import styles from "./Main.module.css";
 
 function Main() {
     return (
         <main className={styles.main}>
             <Navbar />
+            <Dashboard />
         </main>
     );
 }
