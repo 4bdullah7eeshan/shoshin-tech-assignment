dc39a1302a6831ea781b1e5590d2941ae60ede7a
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-12-31 18:09:51 +0530

    feat: complete the user profile menu

--- src/components/UserProfileMenu/UserProfileMenu.jsx
+++ src/components/UserProfileMenu/UserProfileMenu.jsx
@@ -1,10 +1,15 @@
+import NotificationBell from "../NotificationBell/NotificationBell";
+import MessageIcon from "../MessageIcon/MessageIcon";
+import UserProfile from "../UserProfile/UserProfile";
 import styles from "./UserProfileMenu.module.css";
+import user from "../../utils/data/user";
 
 function UserProfileMenu() {
     return (
         <header className={styles.userProfileMenu}>
-            
-
+            <NotificationBell />
+            <MessageIcon />
+            <UserProfile userAvatar={user.userAvatar} userName={user.userName} />
         </header>
     )
 }
