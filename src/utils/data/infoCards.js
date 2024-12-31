59ad17674a6a85db5d7b0ef4a5c8c8291e9173d3
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-12-31 22:39:12 +0530

    chore: setup info cards data

---
+++ src/utils/data/infoCards.js
@@ -0,0 +1,19 @@
+const infoCards = [
+  {
+    title: "Available Position",
+    stat: "24",
+    description: "4 Urgently needed",
+  },
+  {
+    title: "Job Open",
+    stat: "10",
+    description: "4 Active hiring",
+  },
+  {
+    title: "New Employees",
+    stat: "24",
+    description: "4 Departments",
+  },
+];
+
+export default infoCards;
