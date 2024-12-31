e1db439e1027a90a69d115dc3c24371429d03c3a
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-12-31 20:04:59 +0530

    feat: setup info card structure
    These are the colored cards.

---
+++ src/components/InfoCard/InfoCard.jsx
@@ -0,0 +1,11 @@
+import styles from "./InfoCard.module.css";
+
+function InfoCard() {
+    <article className={styles.infoCard}>
+        <p></p>
+        <p></p>
+        <p></p>
+    </article>
+}
+
+export default InfoCard;
