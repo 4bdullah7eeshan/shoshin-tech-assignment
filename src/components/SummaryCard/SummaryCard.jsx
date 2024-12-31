b5c6909357e6e7d252d586d076b41c263ef3bc54
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-12-31 20:43:03 +0530

    feat: setup summary cards structure
    The graph ones.

---
+++ src/components/SummaryCard/SummaryCard.jsx
@@ -0,0 +1,16 @@
+import styles from "./SummaryCard.module.css";
+
+function SummaryCard() {
+    return(
+        <article>
+            <section>
+                {/* Textual content */}
+            </section>
+            <section>
+                {/* Graph */}
+            </section>
+        </article>
+    )
+}
+
+export default SummaryCard;
